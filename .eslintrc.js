71808b3273863a277144622f9f283fdf5a110c1f
kk.z <kk.z@qycocu.com>
2023-12-28 15:56:55 +0800

    add resolve in vite.config.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,9 @@ module.exports = {
   },
   rules: {
     // 在这里添加或覆盖 ESLint 规则
+    // 0='off' 关闭规则
+    // 1='warn' 警告，不会导致ESlint失败退出
+    // 2='error' 错误，将导致ESlint校验失败。
     // 例如，关闭控制台语句的警告
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
@@ -37,6 +40,7 @@ module.exports = {
     'no-irregular-whitespace': 1, // 禁止在字符串和注释之外不规则的空白
     'prefer-const': 0, // 要求使用 const 声明那些声明后不再被修改的变量.如果一个变量从不重新分配，使用const声明更好。const 声明告诉读者，“这个变量永远不会被重新分配，”减少认知负荷并提高可维护性。
     'no-use-before-define': 1, // 禁止在变量定义之前使用它们
+    'no-undef': 2,
     'vue/attributes-order': 2, // vue api使用顺序
     'vue/no-multiple-template-root': 0,
     '@typescript-eslint/explicit-module-boundary-types': 0,
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
+            return 'assets/img/[name]-[hash][extname]'
+          }
+
+          if (/\.css$/.test(name ?? '')) {
+            return 'assets/css/[name]-[hash][extname]'
+          }
+          // default value
+          // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+          return 'assets/[name]-[hash][extname]'
+        }
+      }
+    }
+  }
 })
