4733aa3d17e77f9c8532c388ba89bdaad9ae05a1
kk.z <kk.z@qycocu.com>
2024-03-01 09:53:58 +0800

    learn lodash-es

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,69 @@
 <script setup>
+import { uniq, uniqBy, flatMapDeep } from 'lodash-es'
 import HelloWorld from '@/components/HelloWorld.vue'
+
+const arr = [1, 2, 2, 3, 3, 4, 5, 6, 7, 9, 9]
+const newArr = uniq(arr)
+console.log('arr--', arr, newArr)
+
+const arrayOfObjects = [
+  { id: 1, name: 'Alice' },
+  { id: 2, name: 'Bob' },
+  { id: 1, name: 'Alice' },
+  { id: 3, name: 'Carol' }
+]
+
+// 使用 'id' 属性来去重
+const uniqueArray = uniqBy(arrayOfObjects, 'id')
+console.log('obj-arr--', arrayOfObjects, uniqueArray)
+
+// flattenDeep
+const flattenArrayFunc = (array) => {
+  return flatMapDeep(array, (item) => {
+    const { children, ...rest } = item
+    return [rest, ...flattenArrayFunc(children || [])]
+  })
+}
+
+const flattenDeepArr = [
+  {
+    name: 'aa',
+    id: 'aa',
+    children: [
+      {
+        name: 'aa-1',
+        id: 'aa-1'
+      },
+      {
+        name: 'aa-2',
+        id: 'aa-2'
+      }
+    ]
+  },
+  {
+    name: 'bb',
+    id: 'bb',
+    children: [
+      {
+        name: 'bb-1',
+        id: 'bb-1',
+        children: [
+          {
+            name: 'bb-1-1',
+            id: 'bb-1-1'
+          }
+        ]
+      },
+      {
+        name: 'bb-2',
+        id: 'bb-2'
+      }
+    ]
+  }
+]
+
+const flattenArr = flattenArrayFunc(flattenDeepArr)
+console.log('flatten--', flattenDeepArr, flattenArr)
 </script>
 
 <template>
